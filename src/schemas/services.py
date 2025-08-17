68c73c84c53d3878be14f91406e7962542d249e7
oleglokhtin <olohtin@gmail.com>
2025-08-17 11:42:00 +0300

    Required = None

--- src/schemas/services.py
+++ src/schemas/services.py
@@ -6,5 +6,5 @@ class ServiceScheme(BaseModel):
     company_id: int = Field(gt=0)
     service_name: str
     service_start_date: date
-    service_end_date: date
+    service_end_date: date = None
     service_type: int = Field(gt=0)
